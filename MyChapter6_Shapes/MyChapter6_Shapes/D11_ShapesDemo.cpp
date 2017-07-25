3c62ace533f4c7e0a11867098a16a9a0e60a6913
huyixi95 <huyixi1995@sina.com>
2017-07-25 09:34:50 +0800

    2017/7/25 --huyixi 第一次提交

---
+++ MyChapter6_Shapes/MyChapter6_Shapes/D11_ShapesDemo.cpp
@@ -0,0 +1,447 @@
+//***************************************************************************************
+// learning from ShapesDemo.cpp by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Demonstrates drawing simple geometric primitives in wireframe mode.
+//
+// Controls:
+//		Hold the left mouse button down and move the mouse to rotate.
+//      Hold the right mouse button down to zoom in and out.
+//  hr 161 255 388 408 423 442
+//***************************************************************************************
+
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+
+struct Vertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+class ShapesApp : public D3DApp
+{
+public:
+	ShapesApp(HINSTANCE hInstance);
+	~ShapesApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt); //纯虚
+	void DrawScene(); //纯虚
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	void BuildGeometryBuffers();
+	void BuildFX();
+	void BuildVertexLayout();
+
+private:
+	ID3D11Buffer* mVB;
+	ID3D11Buffer* mIB;
+
+	ID3DX11Effect* mFX;
+	ID3DX11EffectTechnique* mTech;
+	ID3DX11EffectMatrixVariable* mfxWorldViewProj;
+
+	ID3D11InputLayout* mInputLayout;
+
+	ID3D11RasterizerState* mWireframeRS;
+
+	// Define transformations from local spaces to world space.
+	XMFLOAT4X4 mSphereWorld[10];
+	XMFLOAT4X4 mCylWorld[10];
+	XMFLOAT4X4 mBoxWorld;
+	XMFLOAT4X4 mGridWorld;
+	XMFLOAT4X4 mCenterSphere;
+
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
+
+	int mBoxVertexOffset;
+	int mGridVertexOffset;
+	int mSphereVertexOffset;
+	int mCylinderVertexOffset;
+
+	UINT mBoxIndexOffset;
+	UINT mGridIndexOffset;
+	UINT mSphereIndexOffset;
+	UINT mCylinderIndexOffset;
+
+	UINT mBoxIndexCount;
+	UINT mGridIndexCount;
+	UINT mSphereIndexCount;
+	UINT mCylinderIndexCount;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+				   PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined (DEBUG)|defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	ShapesApp theApp(hInstance);
+
+	if (!theApp.Init())
+		return 0;
+
+	return theApp.Run();
+}
+
+ShapesApp::ShapesApp(HINSTANCE hInstance)
+:D3DApp(hInstance), mVB(0), mIB(0), mFX(0), mTech(0),
+mfxWorldViewProj(0), mInputLayout(0), mWireframeRS(0),
+mTheta(1.5f*MathHelper::Pi), mPhi(0.1f*MathHelper::Pi), mRadius(15.0f)
+{
+	mMainWndCaption = L"Shapes Demo";
+
+	mLastMousePos.x = 0;
+	mLastMousePos.y = 0;
+
+	XMMATRIX I = XMMatrixIdentity();
+	XMStoreFloat4x4(&mGridWorld, I);
+	XMStoreFloat4x4(&mView, I);
+	XMStoreFloat4x4(&mProj, I);
+
+	XMMATRIX boxScale = XMMatrixScaling(2.0f, 1.0f, 2.0f);
+	XMMATRIX boxOffset = XMMatrixTranslation(0.0f, 0.5f, 0.0f);
+	XMStoreFloat4x4(&mBoxWorld, XMMatrixMultiply(boxScale, boxOffset));
+
+	XMMATRIX centerSphereScale = XMMatrixScaling(2.0f, 2.0f, 2.0f);
+	XMMATRIX centerSphereOffset = XMMatrixTranslation(0.0f, 2.0f, 0.0f);
+	XMStoreFloat4x4(&mCenterSphere, XMMatrixMultiply(centerSphereScale, centerSphereOffset));
+
+	for (int i = 0; i < 5; ++i)
+	{
+		XMStoreFloat4x4(&mCylWorld[i * 2 + 0], XMMatrixTranslation(-5.0f, 1.5f, -10.0f + i*5.0f));
+		XMStoreFloat4x4(&mCylWorld[i * 2 + 1], XMMatrixTranslation(5.0f, 1.5f, -10.0f + i*5.0f));
+
+		XMStoreFloat4x4(&mSphereWorld[i * 2 + 0], XMMatrixTranslation(-5.0f, 3.5f, -10.0f + i*5.0f));
+		XMStoreFloat4x4(&mSphereWorld[i * 2 + 1], XMMatrixTranslation(5.0f, 3.5f, -10.0f + i*5.0f));
+	}
+}
+
+ShapesApp::~ShapesApp()
+{
+	ReleaseCOM(mVB);
+	ReleaseCOM(mIB);
+	ReleaseCOM(mFX);
+	ReleaseCOM(mInputLayout);
+	ReleaseCOM(mWireframeRS);
+}
+
+bool ShapesApp::Init()
+{
+	if (!D3DApp::Init())
+		return false;
+
+	BuildGeometryBuffers();
+	BuildFX();
+	BuildVertexLayout();
+
+	D3D11_RASTERIZER_DESC wireframeDesc; //光栅化描述
+	ZeroMemory(&wireframeDesc, sizeof(D3D11_RASTERIZER_DESC));
+	wireframeDesc.FillMode = D3D11_FILL_WIREFRAME; //线框绘制模式
+	wireframeDesc.CullMode = D3D11_CULL_BACK; //背面消隐模式
+	wireframeDesc.FrontCounterClockwise = false;
+	wireframeDesc.DepthClipEnable = true;
+
+	md3dDevice->CreateRasterizerState(&wireframeDesc, &mWireframeRS); //hr
+
+	return true;
+}
+
+//拖动窗口大小(重新设置投影矩阵)
+void ShapesApp::OnResize()
+{
+	D3DApp::OnResize();
+
+	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+	XMStoreFloat4x4(&mProj, P);
+}
+
+//更新场景
+void ShapesApp::UpdateScene(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = mRadius*sinf(mPhi)*cosf(mTheta);
+	float z = mRadius*sinf(mPhi)*sinf(mTheta);
+	float y = mRadius*cosf(mPhi);
+
+	// Build the view matrix.
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+
+	XMStoreFloat4x4(&mView, V); //更新摄像机观察的方向
+}
+
+void ShapesApp::DrawScene()
+{
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::Blue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	md3dImmediateContext->IASetInputLayout(mInputLayout);
+	md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	md3dImmediateContext->RSSetState(mWireframeRS);
+
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	md3dImmediateContext->IASetVertexBuffers(0, 1, &mVB, &stride, &offset);
+	md3dImmediateContext->IASetIndexBuffer(mIB, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set constants
+
+	XMMATRIX view = XMLoadFloat4x4(&mView);
+	XMMATRIX proj = XMLoadFloat4x4(&mProj);
+	XMMATRIX viewProj = view*proj;
+
+	D3DX11_TECHNIQUE_DESC techDesc;
+	mTech->GetDesc(&techDesc);
+	for (UINT i = 0; i < techDesc.Passes; ++i) //遍历Tech里的每个Pass
+	{
+		// Draw the grid.
+		XMMATRIX world = XMLoadFloat4x4(&mGridWorld);
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<const float*>(&(world*viewProj)));
+		mTech->GetPassByIndex(i)->Apply(0, md3dImmediateContext); //上面设置了世界视图投影矩阵这里要更新应用，并且通知给context
+		md3dImmediateContext->DrawIndexed(mGridIndexCount, mGridIndexOffset, mGridVertexOffset); 
+
+		// Draw the box.
+		world = XMLoadFloat4x4(&mBoxWorld);
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<const float*>(&(world*viewProj)));
+		mTech->GetPassByIndex(i)->Apply(0, md3dImmediateContext);
+		md3dImmediateContext->DrawIndexed(mBoxIndexCount, mBoxIndexOffset, mBoxVertexOffset);
+
+		// Draw center Sphere.
+		world = XMLoadFloat4x4(&mCenterSphere);
+		mfxWorldViewProj->SetMatrix(reinterpret_cast<const float*>(&(world*viewProj)));
+		mTech->GetPassByIndex(i)->Apply(0, md3dImmediateContext);
+		md3dImmediateContext->DrawIndexed(mSphereIndexCount, mSphereIndexOffset, mSphereVertexOffset);
+
+		// Draw the cylinders.
+		for (int j = 0; j < 10; ++j)
+		{
+			world = XMLoadFloat4x4(&mCylWorld[j]);
+			mfxWorldViewProj->SetMatrix(reinterpret_cast<const float*>(&(world*viewProj)));
+			mTech->GetPassByIndex(i)->Apply(0, md3dImmediateContext);
+			md3dImmediateContext->DrawIndexed(mCylinderIndexCount, mCylinderIndexOffset, mCylinderVertexOffset);
+		}
+
+		// Draw the spheres.
+		for (int j = 0; j < 10; ++j)
+		{
+			world = XMLoadFloat4x4(&mSphereWorld[j]);
+			mfxWorldViewProj->SetMatrix(reinterpret_cast<const float*>(&(world*viewProj)));
+			mTech->GetPassByIndex(i)->Apply(0, md3dImmediateContext);
+			md3dImmediateContext->DrawIndexed(mSphereIndexCount, mSphereIndexOffset, mSphereVertexOffset);
+		}
+	}
+
+	mSwapChain->Present(0, 0); //hr
+}
+
+void ShapesApp::OnMouseDown(WPARAM btnState, int x, int y)
+{
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+
+	SetCapture(mhMainWnd); //如果调用了SetCapture，之后无论鼠标的位置在哪，鼠标消息都发给指定的这个窗口，直到调用ReleaseCapture或者调用SetCapture设置另一个窗口为止。
+}
+
+void ShapesApp::OnMouseUp(WPARAM btnState, int x, int y)
+{
+	ReleaseCapture();
+}
+
+void ShapesApp::OnMouseMove(WPARAM btnState, int x, int y)
+{
+	if ((btnState&MK_LBUTTON) != 0) //有左键的拖拽发生
+	{
+		// Make each pixel correspond to a quarter(四分之一) of a degree.
+		float dx = XMConvertToRadians(0.25f*static_cast<float>(x - mLastMousePos.x));
+		float dy = XMConvertToRadians(0.25f*static_cast<float>(y - mLastMousePos.y));
+
+		// Update angles based on input to orbit camera around box.
+		mTheta += dx;
+		mPhi += dy;
+
+		// Restrict the angle mPhi.
+		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
+	}
+	else if ((btnState&MK_RBUTTON) != 0) //有左键的拖拽发生
+	{
+		// Make each pixel correspond to 0.01 uint in the scene.
+		float dx = 0.01f*static_cast<float>(x - mLastMousePos.x);
+		float dy = 0.01f*static_cast<float>(y - mLastMousePos.y);
+
+		// Update the camera radius based on input.
+		mRadius += dx - dy;
+
+		// Restrict the radius.
+		mRadius = MathHelper::Clamp(mRadius, 3.0f, 200.0f);
+	}
+
+	mLastMousePos.x = x;
+	mLastMousePos.y = y;
+}
+
+void ShapesApp::BuildGeometryBuffers()
+{
+	GeometryGenerator::MeshData box;
+	GeometryGenerator::MeshData grid;
+	GeometryGenerator::MeshData sphere;
+	GeometryGenerator::MeshData cylinder;
+
+	GeometryGenerator geoGen;
+	geoGen.CreateBox(1.0f, 1.0f, 1.0f, box);
+	geoGen.CreateGrid(20.0f, 30.0f, 60, 40, grid);
+	geoGen.CreateSphere(0.5f, 20, 20, sphere);
+	geoGen.CreateCylinder(0.5f, 0.3f, 3.0f, 20, 20, cylinder);
+
+	// Cache the vertex offset to each object in the concatenated vertex buffer.
+	mBoxVertexOffset = 0;
+	mGridVertexOffset = box.Vertices.size();
+	mSphereVertexOffset = mGridVertexOffset + grid.Vertices.size();
+	mCylinderVertexOffset = mSphereVertexOffset + sphere.Vertices.size();
+
+	// Cache the index count of each object.
+	mBoxIndexCount = box.Indices.size();
+	mGridIndexCount = grid.Indices.size();
+	mSphereIndexCount = sphere.Indices.size();
+	mCylinderIndexCount = cylinder.Indices.size();
+
+	// Cache the starting index for each object in the concatenated index buffer.
+	mBoxIndexOffset = 0;
+	mGridIndexOffset = mBoxIndexCount;
+	mSphereIndexOffset = mGridIndexOffset + mGridIndexCount;
+	mCylinderIndexOffset = mSphereIndexOffset + mSphereIndexCount;
+
+	UINT totalVertexCount =
+		box.Vertices.size() +
+		grid.Vertices.size() +
+		sphere.Vertices.size() +
+		cylinder.Vertices.size();
+
+	UINT totalIndexCount =
+		mBoxIndexCount +
+		mGridIndexCount +
+		mSphereIndexCount +
+		mCylinderIndexCount;
+
+	//
+	// Extract the vertex elements we are interested in and pack the 
+	// vertices of all the meshes into one vertex buffer.
+	//
+
+	std::vector<Vertex> vertices(totalVertexCount);
+
+	XMFLOAT4 black(0.0f, 0.0f, 0.0f, 1.0f);
+
+	UINT k = 0;
+	for (size_t i = 0; i < box.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = box.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
+
+	for (size_t i = 0; i < grid.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = grid.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
+
+	for (size_t i = 0; i < sphere.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = sphere.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
+
+	for (size_t i = 0; i < cylinder.Vertices.size(); ++i, ++k)
+	{
+		vertices[k].Pos = cylinder.Vertices[i].Position;
+		vertices[k].Color = black;
+	}
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertex)*totalVertexCount;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = &vertices[0];
+	md3dDevice->CreateBuffer(&vbd, &vinitData, &mVB);
+
+	//
+	// Pack the indices of all the meshes into one index buffer.
+	//
+
+	std::vector<UINT> indices;
+	indices.insert(indices.end(), box.Indices.begin(), box.Indices.end());
+	indices.insert(indices.end(), grid.Indices.begin(), grid.Indices.end());
+	indices.insert(indices.end(), sphere.Indices.begin(), sphere.Indices.end());
+	indices.insert(indices.end(), cylinder.Indices.begin(), cylinder.Indices.end());
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT)*totalIndexCount;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = &indices[0];
+	md3dDevice->CreateBuffer(&ibd, &iinitData, &mIB);
+}
+
+void ShapesApp::BuildFX()
+{
+	std::ifstream fin("fx/color.fxo", std::ios::binary);
+
+	fin.seekg(0, std::ios_base::end);
+	int size = (int)fin.tellg();
+	fin.seekg(0, std::ios_base::beg);
+	std::vector<char> compiledShader(size);
+
+	fin.read(&compiledShader[0], size);
+	fin.close();
+
+	D3DX11CreateEffectFromMemory(&compiledShader[0], size, 0, md3dDevice, &mFX);
+
+	mTech = mFX->GetTechniqueByName("ColorTech");
+	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void ShapesApp::BuildVertexLayout()
+{
+	// Create the Vertex input layout.
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[]=
+	{
+		//SemanticName,SemanticIndex,Format,InputSlot,AlignedByteOffset,InputSlotClass,InstanceDataStepRate
+		{ "POSITION",0, DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0},
+		{ "COLOR",0,DXGI_FORMAT_R32G32B32A32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0}
+	};
+
+	// Create the input layout.
+	D3DX11_PASS_DESC passDesc;
+	mTech->GetPassByIndex(0)->GetDesc(&passDesc);
+	md3dDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature,
+		passDesc.IAInputSignatureSize, &mInputLayout);
+}
+
+
+
